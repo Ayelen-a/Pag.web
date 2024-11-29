e021cb8d675840d7a5bad1f8fb4a08da723288c7
Ayelen-a <ayelen.henriquez@meduca.edu.pa>
2024-11-29 17:55:39 -0500

    Create Web.py

---
+++ Web.py
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>La dependencia tegnologica</title>
+    <link rel="stylesheet" href="style.css"
+</head>
+
+<body>
+    <h1>La dependencia tegnologica en la vida de los seres humanos.</h1>
+    <h1>¿Que es?</h2>
+    <p>La dependencia tecnológica, también conocida como adicción a la tecnología, es un comportamiento compulsivo que consiste en usar dispositivos tecnológicos de manera excesiva e inadecuada. Esto puede interferir en las relaciones interpersonales y en las actividades diarias. </p>
+    <h1>Efectos de la dependencia tegnologica en la vida de las personas:</h2>
+             <ul>
+                   <li>Problemas de salud:
+                    Puede causar problemas para dormir, insomnio, cansancio visual, dolor de cabeza, molestias musculares, tendinitis y síndrome del túnel carpiano. </li>
+
+                   <li>Problemas psicológicos:
+                    Puede causar ansiedad, depresión, malestar emocional, estrés, irritabilidad, mal humor, ira y conductas de riesgo. </li>
+
+                    <li>Problemas sociales:
+                        Puede dificultar la socialización presencial, deteriorar las relaciones sociales o incluso romperlas. </li>
+
+                    <li>Problemas académicos y laborales
+                        Puede disminuir el rendimiento académico o laboral. </li>    
+
+                    <li>Problemas de memoria
+                        Puede dificultar la concentración y la atención, y dificultar la memoria.</li>
+
+                    <li>Nomofobia
+                        Puede causar un temor irracional a quedarse sin el móvil o no poder usarlo.</li>
+
+                        
+             </ul>
+
+            
+    <h1>En la siguiente imagen podemos ver un ejemplo de lo que es la dependencia tegnologica:</h2>
+    <img src="https://img.blogs.es/bloglenovo/wp-content/uploads/2019/08/dependencia-tecnologica.jpg" alt="Image 1">
+    <p>Esta es muy peligrosa para nuestro bienestar tanto fisico como mental, tambien puede afectar nuestra relacion con nuestros familiares o amigos ya que esta nos lleva a un aislamiento como es observada en la imagen.</p>
+
+    <h1>¿Que podemos hacer para disminuir el uso de dispositivos tegnologicos?</h1>
+    <p>Se pueden tomar muchas medidas para evitar el uso excesivo de esto aparatos las cuales son:</p>
+
+        <ul>
+            <li>Establecer limites en el uso de los dispositivos.</li>
+
+            <li>Evitar las distracciones mientras se realizan actividades en familia o que sean sin dispositivos.</li>
+
+            <li>Practicar el autocontrol, esto consiste en reconocer que se esta excediendo el uso de los dispositivos.</li>
+
+        </ul>
+    <h1>Muchas gracias por leer :D </h1>
+
+</body>
+</html>
